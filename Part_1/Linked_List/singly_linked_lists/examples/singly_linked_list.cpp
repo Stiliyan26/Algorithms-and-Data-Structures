a6bb96d8866a878d69ab9d79d9d36beb37ecfb7b
Stiliyan26 <stili262002@abv.bg>
2024-12-29 18:35:44 +0200

    Simply Linked List full impl get method done

--- Part_1/Linked_List/singly_linked_lists/examples/singly_linked_list.cpp
+++ Part_1/Linked_List/singly_linked_lists/examples/singly_linked_list.cpp
@@ -1,21 +1,49 @@
 #include <iostream>
 
+struct ListNode {
+  int val;
+  ListNode* next;
 
-struct ListNode
-{
-  int val_;
-  ListNode *next = nullptr;
+  ListNode(int _val) : val(_val), next(nullptr) {}
 
-  ListNode(int val)
-  {
-    val_ = val;
-  }
+  ListNode(int _val, ListNode* _next) : val(_val), next(_next) {}
 };
 
-class SinglyLinkedList
-{
+class SinglyLinkedList {
+
+private:
+  ListNode* head;
+  ListNode* tail;
+
 public:
-  
+  SinglyLinkedList() {
+    head = new ListNode(-1);
+    tail = head;
+  }
+
+
+  int get(int index) {
+    int currentIndex = 0;
+    ListNode* currentNode = head->next;
+
+    while (!equalCurrentIndexAndIndex(currentIndex, index) && !nodeIsNullptr(currentNode)) {
+      currentIndex++;
+      currentNode = currentNode->next;
+    }
+
+    if (nodeIsNullptr(currentNode)) return -1;
+
+    return currentNode->val;
+  }
+
+private:
+  bool equalCurrentIndexAndIndex(int currentIndex, int index) {
+    return currentIndex == index;
+  }
+
+  bool nodeIsNullptr(ListNode* node) {
+    return node == nullptr;
+  }
 };
 
 int main() {
@@ -24,4 +52,3 @@ int main() {
 
   return 0;
 }
-
